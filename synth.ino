04c7fa21f16a33a38ad256e91f0ba538676c6b17
benschnuck <benschnuck@users.noreply.github.com>
2018-08-02 10:41:50 -0500

    Create synth.ino

---
+++ synth.ino
@@ -0,0 +1,49 @@
+#include <Bounce.h>
+#include <Keypad.h>
+#include <Metro.h>
+
+Metro beat = Metro(500);
+
+bool kick1Steps[16]{false};
+bool kick2Steps[16]{false};
+bool kick3Steps[16]{false};
+bool kick4Steps[16]{false};
+bool snare1Steps[16]{false};
+bool snare2Steps[16]{false};
+bool snare3Steps[16]{false};
+bool snare4Steps[16]{false};
+bool hh1Steps[16]{false};
+bool hh2Steps[16]{false};
+bool hh3Steps[16]{false};
+bool hh4Steps[16]{false};
+bool clap1Steps[16]{false};
+bool clap2Steps[16]{false};
+bool clap3Steps[16]{false};
+bool clap4Steps[16]{false};
+
+void setup()
+{
+  
+}
+void loop()
+{
+  for (int j=0; j<4; j++)
+  {
+    if(j == 0)
+    {
+      
+    }
+    if(j == 1)
+    {
+      //buttons and switches
+    }
+    if(j == 2)
+    {
+      //keypad
+    }
+    if(j == 3)
+    {
+      
+    }
+  }
+}
