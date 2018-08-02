876b9e773da83ebaddccbc43168960a1a71467e8
benschnuck <benschnuck@users.noreply.github.com>
2018-08-02 11:54:57 -0500

    Update synth.ino

--- synth.ino
+++ synth.ino
@@ -4,6 +4,8 @@
 
 Metro beat = Metro(500);
 
+bool seqSwitch = false;
+bool playControl = false;
 bool kick1Steps[16]{false};
 bool kick2Steps[16]{false};
 bool kick3Steps[16]{false};
@@ -20,6 +22,26 @@ bool clap1Steps[16]{false};
 bool clap2Steps[16]{false};
 bool clap3Steps[16]{false};
 bool clap4Steps[16]{false};
+
+//[x][y] x refers to row, y refers to column
+bool drumSteps[16][16] {
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}
+};
 
 void setup()
 {
@@ -43,7 +65,83 @@ void loop()
     }
     if(j == 3)
     {
-      
+      if(seqSwitch)
+      {
+          
+      }
+      if(playControl)
+      {
+        for(i=0; i<17; i++)
+        {
+            if(beat.check())
+            {
+              if(kick1Steps[i])
+              {
+                  
+              }
+              if(kick2Steps[i])
+              {
+                  
+              }
+              if(kick3Steps[i])
+              {
+                  
+              }
+              if(kick4Steps[i])
+              {
+                  
+              }
+              if(snare1Steps[i])
+              {
+                  
+              }
+              if(snare2Steps[i])
+              {
+                  
+              }
+              if(snare3Steps[i])
+              {
+                  
+              }
+              if(snare4Steps[i])
+              {
+                  
+              }
+              if(hh1Steps[i])
+              {
+                  
+              }
+              if(hh2Steps[i])
+              {
+                  
+              }
+              if(hh3Steps[i])
+              {
+                  
+              }
+              if(hh4Steps[i])
+              {
+                  
+              }
+              if(clap1Steps[i])
+              {
+                  
+              }
+              if(clap1Steps[i])
+              {
+                  
+              }
+              if(clap1Steps[i])
+              {
+                  
+              }
+              if(clap1Steps[i])
+              {
+                  
+              }
+            }
+        }
+      }
     }
   }
 }
